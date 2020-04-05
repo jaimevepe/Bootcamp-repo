0611cbc7f9fcc2049098bc67dbbe8fad2049c2d1
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-04-05 15:35:46 -0500

    Bitcoin Proj

--- Node EJS Projects/Bitcoin_API/app.js
+++ Node EJS Projects/Bitcoin_API/app.js
@@ -23,12 +23,13 @@ app.get('/price', (req, res)=> {
     })
     .then(data => {
         let rate = data.bpi[req.query.currency].rate_float.toFixed(2);
+        let symbol = data.bpi[req.query.currency].symbol;
         console.log(rate);
-        // res.render("index.ejs", {rate: rate});
+        res.render("index.ejs", { rate, symbol });
     })
     .catch(error => {
         console.error("Error from network");
-        // res.render("index.ejs", {error : "There was an Error"});
+        res.render("index.ejs", {error : "There was an Error"});
     })
 })
 
