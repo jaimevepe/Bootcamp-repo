4bf50a6b45a9953ae8d3e7fd14ef9c65045d5a77
jaime velazquez <jaimevelazquez@Admins-MacBook-Pro.local>
2020-04-05 22:11:09 -0500

    Bitcoin

--- Node EJS Projects/Bitcoin_API/app.js
+++ Node EJS Projects/Bitcoin_API/app.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 
 const $fetch = require('node-fetch');
 
-let url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+let url = "https://api.coindesk.com/v1/bpi/currentprice.json" // TODO: API endpoint
 
 const port = process.env.PORT || 3000;
 
@@ -16,22 +16,22 @@ app.get('/', (req, res) => {
 app.get('/price', (req, res)=> {
     $fetch(url)
     .then(response =>{
-        if(!response.ok){
+        if(!response.ok){ // TODO: if not ok, stop and thro an error
             throw Error(response.statusText);
         }
-        return response.json();
+        return response.json(); //TODO: extract data from response and move to the next
     })
     .then(data => {
-        let rate = data.bpi[req.query.currency].rate_float.toFixed(2);
-        let symbol = data.bpi[req.query.currency].symbol;
+        let rate = data.bpi[req.query.currency].rate_float.toFixed(2); // TODO: currency is the name="currency" on inputs
+        let symbol = data.bpi[req.query.currency].symbol;    // Always have the value attribute on input or else data wont be sent
         console.log(rate);
-        res.render("index.ejs", { rate, symbol });
+        res.render("index.ejs", { rate, symbol }); //TODO: same as {rate: rate} and {symbol: symbol}
     })
     .catch(error => {
         console.error("Error from network");
         res.render("index.ejs", {error : "There was an Error"});
-    })
-})
+    });
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
