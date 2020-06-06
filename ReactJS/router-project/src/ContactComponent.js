00c7509a7415d4e7bb699c2183d430583533f713
jaime velazquez <jaimevelazquez@Admins-MBP.attlocal.net>
2020-06-06 09:00:17 -0500

    React routing lecture

---
+++ ReactJS/router-project/src/ContactComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactComponent = () => {
+    return(
+            <div>
+                <h1>Contact View</h1>
+                <img src="https://i.imgur.com/lcRArkB.gif" width="70%" alt="comic"/>
+            </div>
+    )
+    }
+
+export default ContactComponent;
